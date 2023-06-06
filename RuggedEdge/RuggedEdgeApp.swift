9fa3e93127e441c355a23cf3a55fb0f393cf870b
Christopher Jones <cajlondon86@gmail.com>
2023-06-06 10:23:31 -0500

    Initial Commit

---
+++ RuggedEdge/RuggedEdgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuggedEdgeApp.swift
+//  RuggedEdge
+//
+//  Created by Christopher Jones on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RuggedEdgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
